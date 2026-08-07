ee9e3546a9044393b1c06e9c43335b40314db88a
Prasad-prajapat <prajapatpunamchand90@gmail.com>
2026-08-07 12:17:40 +0530

    user registration done

---
+++ Backend/services/user.service.js
@@ -0,0 +1,21 @@
+const userModel = require('../models/user.model')
+
+async function createUser( { firstname, lastname, email, password } ){
+    
+    if( !firstname || !email || !password ){
+        throw new Error("All field are required");
+    }
+
+    const user = userModel.create({
+        fullname: {
+            firstname,
+            lastname
+        },
+        email,
+        password
+    })
+
+    return user
+}
+
+module.exports = { createUser }
